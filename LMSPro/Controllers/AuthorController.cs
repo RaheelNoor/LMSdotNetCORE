de188debb8f6cedeb992eccf1e6dcdf37a2f6215
nraheel34 <nraheel34@gmail.com>
2025-02-10 15:57:36 -0800

    Just Test for push code

--- LMSPro/Controllers/AuthorController.cs
+++ LMSPro/Controllers/AuthorController.cs
@@ -35,7 +35,7 @@ namespace LMSPro.Controllers
         {
             return _authorService.PostAuthor(author);
         }
-
+        // i am writing this commit for test git work 
         [HttpPut("{id}")]
         public IActionResult UpdateAuthor(int id, Author author)
         {
@@ -51,83 +51,5 @@ namespace LMSPro.Controllers
             if (!deleted) return NotFound();
             return NoContent();
         }
-
-
-
-
-
-
-
-
-        //private readonly ApplicationDbContext _context;
-
-        //public AuthorsController(ApplicationDbContext context)
-        //{
-        //    _context = context;
-        //}
-
-        //// GET: api/authors
-        //[HttpGet]
-        //public async Task<ActionResult<IEnumerable<Author>>> GetAuthors()
-        //{
-        //    return Ok(await _context.Authors.ToListAsync());
-        //}
-
-        //// GET: api/authors/5
-        //[HttpGet("{id}")]
-        //public async Task<ActionResult<Author>> GetAuthor(int id)
-        //{
-        //    var author = await _context.Authors.FindAsync(id);
-        //    if (author == null)
-        //        return NotFound(new { message = "Author not found" });
-
-        //    return Ok(author);
-        //}
-
-        //// POST: api/authors (Create)
-        //[HttpPost]
-        //public async Task<ActionResult<Author>> CreateAuthor([FromBody] Author author)
-        //{
-        //    if (author == null)
-        //        return BadRequest(new { message = "Invalid author data" });
-
-        //    _context.Authors.Add(author);
-        //    await _context.SaveChangesAsync();
-
-        //    return CreatedAtAction(nameof(GetAuthor), new { id = author.AuthorID }, author);
-        //}
-
-        //// PUT: api/authors/5 (Update)
-        //[HttpPut("{id}")]
-        //public async Task<IActionResult> UpdateAuthor(int id, [FromBody] Author author)
-        //{
-        //    if (id != author.AuthorID)
-        //        return BadRequest(new { message = "Author ID mismatch" });
-
-        //    var existingAuthor = await _context.Authors.FindAsync(id);
-        //    if (existingAuthor == null)
-        //        return NotFound(new { message = "Author not found" });
-
-        //    existingAuthor.FirstName = author.FirstName;
-        //    existingAuthor.LastName = author.LastName;
-        //    existingAuthor.Bio = author.Bio;
-
-        //    await _context.SaveChangesAsync();
-        //    return Ok(new { message = "Author updated successfully" });
-        //}
-
-        //// DELETE: api/authors/5 (Delete)
-        //[HttpDelete("{id}")]
-        //public async Task<IActionResult> DeleteAuthor(int id)
-        //{
-        //    var author = await _context.Authors.FindAsync(id);
-        //    if (author == null)
-        //        return NotFound(new { message = "Author not found" });
-
-        //    _context.Authors.Remove(author);
-        //    await _context.SaveChangesAsync();
-
-        //    return Ok(new { message = "Author deleted successfully" });
-        //}
     }
 }
